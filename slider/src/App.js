68b0f550a47b0a4da28e91f9c5a0d80cf7a9aac0
unknown <webdevshokota@yandex.ru>
2018-07-07 09:17:30 +0300

    Это слайдер

---
+++ slider/src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Row from 'react-bootstrap/lib/Button';
+import Grid from 'react-bootstrap/lib/Button';
+import Col from 'react-bootstrap/lib/Button';
+import styled from 'styled-components';
+
+class App extends Component {
+  render() {
+    return (
+              <div className="wrapper">
+                <SayFullName name="Имя" surname="Фaмилия" link="vk.com" />
+                <SayFullName name="Name" surname="Surname" link="#" />
+                <SayFullName name="Даниил" surname="Шокота" link="https://magnum13l.000webhostapp.com/" />
+              </div>
+    );
+  }
+}
+
+function SayFullName(props) {
+  return(
+    <div>
+      <h1> Мое имя {props.name}, фамилия - {props.surname}</h1>
+      <a href={props.link}> Ссылка на мой профиль</a>
+    </div>
+    )
+}
+
+export default App;
